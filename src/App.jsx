a30edede8610039a3fc86dbdc0f936c86111e350
Beeve <ckc40c@gmail.com>
2025-07-25 01:39:52 -0700

    Added Sliders
    tried for a while to add a notebook with pages of past found solutions
    It worked, but looked very ugly so I deleted it. I then decided to add sliders that dictate the seed and thus result you get.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ function App() {
 
   const [result, setResult] = useState(null)
   const [ loading, setLoading ] = useState(false)
+  const [ minX, setMinX ] = useState(1000)
+  const [ maxX, setMaxX ] = useState(3000)
+  const [ minN, setMinN ] = useState(1000)
+  const [ maxN, setMaxN ] = useState(3000)
    
   const handleClick = () => {
     if (result) {
@@ -16,7 +20,7 @@ function App() {
     }
     setLoading(true)
     setTimeout(() => {
-       const nearMiss = findNearMiss()
+       const nearMiss = findNearMiss({minX, maxX, minN, maxN})
       setResult(nearMiss)
       setLoading(false)
     }, 100) // Simulate loading time 
@@ -54,11 +58,54 @@ function App() {
        ) }
          
 
-        {!loading && result === null && <p>No near miss found.</p>}
+        {!loading && result === null && <p>No near miss found.</p>} 
 
         <button onClick={handleClick}> {result ? "Find Another " : "Find Near Miss" } </button>
       </div>
-    </> 
+      
+      <div className="inputs">
+        <label>
+          Range for x and y seed:
+          <input 
+            type="number"
+            value={minX}
+            onChange={(e) => setMinX(Number(e.target.value))} />
+            to
+            <input 
+            type="number"
+            value={maxX}
+            onChange={(e) => setMaxX(Number(e.target.value))} />
+        </label>
+        <br/>
+        <label>
+          Range for n:
+          <input 
+            type="number"
+            value={minN}
+            onChange={(e) => setMinN(Number(e.target.value))} />  
+            to
+            <input 
+            type="number"
+            value={maxN}
+            onChange={(e) => setMaxN(Number(e.target.value))} />
+        </label>
+      </div>
+
+      <motion.div 
+        className="carousel"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >  
+        <p className="notebook">
+          Fermat's Last Theorem states that there are no three positive integers a, b, and c that satisfy the equation: 
+           <InlineMath math = {`a^n + b^n = c^n`} /> for any integer value of n greater than 2.
+          However, near misses can occur when the left-hand side is very close to the right-hand side, but not exactly equal. </p>
+   
+        </motion.div>
+
+       
+    </>  
   ) 
 }
 
--- src/Math.jsx
+++ src/Math.jsx
@@ -1,13 +1,14 @@
 
-function findNearMiss(){
+
+function findNearMiss({minX, maxX, minN, maxN}) {
 
     let tolerance = 0.0001
-    let xRand = Math.floor(Math.random() * 1000) + 1000;
+    let xRand = Math.floor(Math.random() * maxX-minX) + minX;
       
  
     for(let x = xRand; x < xRand+2000; x++) {
         for(let y = xRand ; y < xRand + 2000; y++) {     
-            for(let n = 3; n <= 10; n++) {  
+            for(let n = minN; n <= maxN; n++) {  
                     let a = Math.pow(x, n)
                     let b = Math.pow(y, n)
                     let idealC = Math.pow((a+b), (1/n))
